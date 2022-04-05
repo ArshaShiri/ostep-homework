07960a416937c601d5c75704f395ce12c62aaff7
Arsha <arsha.shiri@gmail.com>
2022-04-05 16:58:46 +0200

    Added the program for the 2nd problem.

---
+++ cpu-api/2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+  FILE *file = fopen("../p2.txt", "w+");
+
+  char line1[] = "Before forking\n";
+  fputs(line1, file);
+
+  // Force flushing the buffer before forking!
+  fflush(file);
+
+  int rc = fork();
+  char line2[] = "After forking\n";
+  fputs(line2, file);
+
+  if (rc < 0 || file < 0) {
+    fprintf(stderr, "Fork failed\n");
+    exit(1);
+  } else if (rc == 0) {
+    char line3[] = "I am the child process!\n";
+    fputs(line3, file);
+  } else {
+    char line4[] = "I am the parent process!\n";
+    fputs(line4, file);
+  }
+  return 0;
+}
